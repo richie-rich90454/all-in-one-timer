d4d1831154ad6734aafa1969824d98ee09552d68
richie-rich90454 <zhiyu.jiang90454-bisz@basischina.com>
2025-02-26 19:56:28 +0800

    implemented backend #1

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
-function setTimer(){
-    var h=document.getElementById
+let swInterval, cdInterval;
+let swRun=false;
+let swH=0, swM=0, swS=0;
+let cdH=0, cdM=0, cdS=0;
+let stopwatchH=document.getElementById("stopwatch-h");
+let stopwatchM=document.getElementById("stopwatch-m");
+let stopwatchS=document.getElementById("stopwatch-s");
+function formatTime(time){
+    if (time<10){
+        return "0"+time;
+    }
+    else{
+        return time;
+    }
 }
+function updateSWDisplay(){
+    stopwatchH.textContent=formatTime(swH)+":";
+    stopwatchM.textContent=formatTime(swM)+":";
+    stopwatchS.textContent=formatTime(swS);
+}
+document.getElementById("start-stopwatch").addEventListener("click", function(){
+    if (swRun){
+        clearInterval(swInterval);
+        this.textContent="START STOPWATCH";
+    }
+    else{
+        swInterval=setInterval(function(){
+            swS++;
+            if (swS==60){
+                swS=0;
+                swM++;
+            }
+            if (swM==60){
+                swM=0;
+                swH++;
+            }
+            updateSWDisplay();
+        },1000);
+        this.textContent="STOP STOPWATCH";
+    }
+});
